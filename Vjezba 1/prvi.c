5ede971755790b6d12fc71ee2091514594181661
Mutija <mateojuretic10@gmail.com>
2025-10-09 21:01:38 +0200

    napravljeno brojanje redaka

--- Vjezba 1/prvi.c
+++ Vjezba 1/prvi.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
 int main() {
-    printf("Poruka\n");
+
+    FILE *fo; //pokazivac za otvaranje datoteke
+
+    int count = 0; //varijabla brojaca
+
+    char c; //trenutni character
+
+    fo = fopen("student.txt", "r");
+
+    if(fo == NULL){
+
+        printf("Datoteka se ne moze otvoriti!");
+
+    }
+
+    //for petlja za brojanje svih redaka u datoteci
+    for(; (c = getc(fo)) != EOF;){
+
+        if(c == '\n'){
+
+            count++;
+
+        }
+
+    }
+
+    printf("Broj redaka je: %d", count);
+
     return 0;
 }
