54975b5c86b26aa5fca1d5ce605555ffdb5aeaad
Mutija <mateojuretic10@gmail.com>
2025-10-10 10:04:40 +0200

    dinamicka alokacija + ucitavanje iz datoteke

--- Vjezba 1/prvi.c
+++ Vjezba 1/prvi.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include<stdlib.h>
+
+typedef struct{
+    
+    char ime[20];
+    char prezime[50];
+    int bodovi;
+
+}student;
+
+
 
 int main() {
 
     FILE *fo; //pokazivac za otvaranje datoteke
+
+    student *stud; //pokazivac na strukturu
 
     int count = 0; //varijabla brojaca
 
@@ -27,6 +40,22 @@ int main() {
 
     }
 
+    //vracanje na pocetak datoteke da bi mogli ponovo krenuti od pocetka u for petlji
+    rewind(fo);
 
+    //dinamicka alokacija za niz struktura
+    stud = (student *)malloc(sizeof(student) * count);
+
+    //ucitavanje podataka iz datoteke
+    for (int i = 0; i < count; i++)
+    {
+        fscanf(fo, "%s %s %d", stud[i].ime, stud[i].prezime, &stud[i].bodovi);
+    }
+    
+    for (int i = 0; i < count; i++)
+    {
+        printf("%s %s: %d\n", stud[i].ime, stud[i].prezime, stud[i].bodovi);
+    }
+    
     return 0;
 }
